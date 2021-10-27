69937dc81c4f64a5569ac1c8c1830fa7386e97c7
suganthiparthi09 <sugansuganthi18@gmail.com>
2021-10-27 14:11:29 +0530

    firstcommit

---
+++ src/org/login/Employee.java
@@ -0,0 +1,13 @@
+package org.login;
+
+public class Employee {
+	
+public static void main(String[] args) {
+	
+	StudentDetails.studId();
+	studId
+}
+	
+}
+
+
---
+++ src/org/login/StudentDetails.java
@@ -0,0 +1,16 @@
+package org.login;
+
+public class StudentDetails {
+
+	public static void studId() {
+		System.out.println("stud id is 123");
+int id =10;
+System.out.println(id);
+	}
+	
+	public static void main(String[] args) {
+		studId();
+		
+		
+	}
+}
